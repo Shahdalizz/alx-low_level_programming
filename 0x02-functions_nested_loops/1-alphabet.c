66b9808bc283367b9980ee3f2a3058bc0ae7ae4f
shahdalizz <shahd096ali@gmail.com>
2023-09-12 13:45:40 -0700

    tassk 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,8 @@
  * print_alphabet - print all alphabet in lowercase
  */
 void print_alphabet(void)
-{	
+{
 	char letter;
-        	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	_putchar(letter);
 	_putchar('\n');
