6f14d1a4ead6a19b9d80bfe78b3150a281049d02
shahdalizz <shahd096ali@gmail.com>
2023-09-13 07:24:44 -0700

    updat task 2 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,6 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -41,9 +40,6 @@ _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
-}
-}
+}}
 _putchar('\n');
-}
-}
-}
+}}}
