02f00368ce036d74bb7c66b26ef3f4b54821759c
shahdalizz <shahd096ali@gmail.com>
2023-09-18 14:46:11 -0700

    update 2 task 4

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
@@ -10,7 +9,6 @@ int main(void)
 	char *str;
 
 	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-
 	print_rev(str);
 	return (0);
 }
