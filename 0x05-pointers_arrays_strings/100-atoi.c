bc3a73871e0cf65578ff2af75c440be595652305
shahdalizz <shahd096ali@gmail.com>
2023-09-18 18:07:24 -0700

    update task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,16 +26,16 @@ int _atoi(char *s)
 		++d;
 	if (s[i] >= '0' && s[i] <= '9')
 	{
-	digit = s[i] - '0';
+		digit = s[i] - '0';
 	if (d % 2)
 		digit = -digit;
 		n = n * 10 + digit;
-	f = 1;
+		f = 1;
 	if (s[i + 1] < '0' || s[i + 1] > '9')
 		break;
-	f = 0;
+		f = 0;
 	}
-	i++;
+		i++;
 	}
 	if (f == 0)
 	return (0);
