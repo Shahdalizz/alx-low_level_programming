7169996bc9e7c96eda60fb298ca04c76e95d1a22
shahdalizz <shahd096ali@gmail.com>
2023-09-18 17:24:52 -0700

    task6 update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,6 @@
  * Return: print
  */
 void puts2(char *str)
-
 {
 	int longi = 0;
 	int t = 0;
@@ -16,15 +15,15 @@ void puts2(char *str)
 
 	while (*y != '\0')
 	{
-		y++;
-		longi++;
+	y++;
+	longi++;
 	}
-		t = longi - 1;
-		for (o = 0 ; o <= t ; o++)
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
 	{
-		if (o % 2 == 0)
+	if (o % 2 == 0)
 	{
-		_putchar(str[o]);
+	_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
